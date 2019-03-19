36a7d69d3c9bb0f256278f92c233512aad40242f
Daneris Cruz <daneriscruz@Daneriss-MacBook-Air.local>
2019-03-18 20:32:50 -0400

    assignment 2964 complete

--- spec/contact_controller_spec.js
+++ spec/contact_controller_spec.js
@@ -20,10 +20,11 @@ const sequelize = require("../db/models/index").sequelize;
    describe("#addContact()", () => {
 
      it("should add a single contact into the book", () => {
-       this.book.addContact("Alice", "484-869-4499")
+       this.book.addContact("Alice", "484-869-4499","alice123@gmail.com")
         .then((contact) =>{
           expect(contact.name).toBe("Alice");
           expect(contact.phone).toBe("484-869-4499");
+          expect(contact.email).toBe("alice123@gmail.com");
           done()
         })
         .catch((err) =>{
